fcf6eaedf0c87c9aaa5a71259e15235d8c1c98aa
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-02-22 15:38:31 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+puts 'Destroyed all restaurants...'
+
+puts 'Start creating restaurants...'
+
+5.times {
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  phone = Faker::PhoneNumber.phone_number_with_country_code
+  category = %w(chinese italian japanese french belgian).sample
+  Restaurant.create(name: name, address: address, phone_number: phone, category: category)
+  puts 'created an restaurant'
+}
+
+puts 'Finished!'
