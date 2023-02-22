4172678038ae8b2469a5f32920f3fbaec29cdea2
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-02-22 11:02:52 +0000

    validations done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
+
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
+
+
+# A review must belong to a restaurant.
+# A review must have a content.
+# A review must have a rating.
+# A review’s rating must be a number between 0 and 5.
+# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
